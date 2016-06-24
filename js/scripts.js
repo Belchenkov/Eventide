8222a015b985a525955c98283c6fa105114997b9
Belchenkov <u608110@gmail.com>
2016-06-24 17:51:53 +0300

    + add Form --- select2

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(function(){
 	$('.collapse').on('show.bs.collapse', function () {
         $(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle');
-    });
+    });// end on()
     $('.collapse').on('hide.bs.collapse', function () {
         $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
-    })
+    });// end on()
+    $('.form-sel').select2({
+        placeholder: "Select a State",
+        allowClear: true
+    });
 });
